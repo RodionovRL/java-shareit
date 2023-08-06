f7c0199b0dbc3a2e9cf1b521ef4ee025feadb361
rodionov ruslan <rodionov.R.l@gmail.com>
2023-08-06 19:03:29 +0300

    fix: fix problems with BookingInputDto

--- src/main/java/ru/practicum/shareit/booking/dto/BookingInputDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingInputDto.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.booking.dto;
 
 import lombok.Builder;
 import lombok.Data;
-import org.springframework.validation.annotation.Validated;
 
 import javax.validation.constraints.Future;
 import javax.validation.constraints.FutureOrPresent;
@@ -10,16 +9,15 @@ import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Data
-@Validated
 @Builder
 public class BookingInputDto {
-    private final long id;
+    private long id;
     @NotNull
     @FutureOrPresent(message = "Start booking may be only in present or future")
-    private final LocalDateTime start;
+    private LocalDateTime start;
     @NotNull
     @Future(message = "End of booking may be only in future")
-    private final LocalDateTime end;
+    private LocalDateTime end;
     @NotNull
-    private final long itemId;
+    private long itemId;
 }
